af4f882f14e464716b5682e56f8b6c093cfb1f24
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-07-13 02:14:45 +1000

    adding Item

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { nanoid } from 'nanoid';
 import Form from './components/Form';
 import List from './components/List';
 
 function App() {
   const [items, setItems] = useState([]);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    setItems([...items, newItem]);
+  };
   return (
     <section className="section-center shadow-lg ">
-      <Form />
+      <Form addItem={addItem} />
     </section>
   );
 }
