d03f847c9f93cb7fbbc8fd04a8caf5160c6749d5
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-07-13 03:51:37 +1000

    single item

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,7 +3,7 @@ import SingleList from './SingleList';
 
 const List = ({ items, removeList }) => {
   return (
-    <div className="bg-white mt-5">
+    <div className="mt-5">
       {items.map((item) => {
         return <SingleList key={item.id} item={item} removeList={removeList} />;
       })}
--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SingleList = () => {
-  return <h2>SingleList</h2>;
+const SingleList = ({ item, removeList }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
+  return (
+    <div className="single-item my-3">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{ textDecoration: isChecked && 'line-through' }}
+        className="text-capitalize"
+      >
+        {item.name}
+      </p>
+      <div className="btn-container d-flex ">
+        <button className="btn btn-warning edit-btn">Edit</button>
+        <button className="btn btn-danger remove-btn">Delete</button>
+      </div>
+    </div>
+  );
 };
 
 export default SingleList;
